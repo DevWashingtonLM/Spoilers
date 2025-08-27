cd552c0e3a89844a755044511f5fd4a4fc228c2a
Washington Medeiros <washingtonshild@gmail.com>
2025-08-27 15:23:35 -0300

    update song

--- footer.js
+++ footer.js
@@ -1,4 +1,19 @@
-document.write(`<footer class="bg-dark text-white text-center p-3 mt-4 copy">
+
+document.write(`
+    <script>
+    const audio = document.getElementById("meuAudio");
+    const muteBtn = document.getElementById("muteBtn");
+  
+  
+    audio.volume = 0.05;
+  
+    muteBtn.addEventListener("click", () => {
+        audio.muted = !audio.muted;
+        muteBtn.textContent = audio.muted ? "🔇 Desmutar" : "🔊 Mutar";
+    });
+    </script>
+    
+    <footer class="bg-dark text-white text-center p-3 mt-4 copy">
         &copy; 2025 Otaku Em Minutos. Todos os spoilers reservados.
     </footer>
 </body>
